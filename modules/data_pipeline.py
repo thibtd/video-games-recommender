0b9b1bcea9bdf3c11124026e0deff77e53eb8519
thibtd <thibautdonis1998@gmail.com>
2024-10-24 20:09:53 +0200

    some more refactoring, made some tests, functionning streamlit app

---
+++ modules/data_pipeline.py
@@ -0,0 +1,24 @@
+# Pipeline for data retrieval, preprocessing, and feature engineering.
+
+import pandas as pd
+import numpy as np
+import kagglehub 
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.text import TfidfVectorizer 
+
+
+def get_data():
+    pass 
+def clean_data(df):
+    pass
+def feature_engineering(df):
+    pass 
+
+def tf_idf_names(df):
+    pass
+
+def main():
+    pass 
+
+if __name__ == "__main__":
+    main()
